f069c0f8a2f46adeda4273b142d3cacaa08c7335
Mokadi Surya Prasad <90204775+mokadi-suryaprasad@users.noreply.github.com>
2025-01-20 19:11:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pods_id=$(microk8s kubectl get pods -n react-nginx |grep react |awk {'print $1'})
-microk8s kubectl delete pod $pods_id -n react-nginx 
+pods_id=$(microk8s kubectl get pods -n react-microk8s |grep react |awk {'print $1'})
+microk8s kubectl delete pod $pods_id -n react-microk8s 
