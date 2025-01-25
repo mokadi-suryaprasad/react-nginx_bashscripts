2a066a9d46b6b3767cf8356682fbadeed56b85bc
Mokadi Surya Prasad <90204775+mokadi-suryaprasad@users.noreply.github.com>
2025-01-25 18:12:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am deleting existing images to save space
-sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
+sudo docker container prune -f && sudo docker image prune -a -f && sudo docker volume prune -f && sudo docker network prune -f && sudo docker system prune -a -f
+##this above step is not recommned step, i am deleting existing images to save space
+sudo rm -r gold
 sudo mkdir gold
 cd gold/
 sudo git clone https://github.com/mokadi-suryaprasad/Gold_Site_Ecommerce.git
-cd Gold_Site_Ecommerce/
-sudo docker build -t react-nginx -f golddockerfile .
-sudo docker tag react-nginx:latest suryaprasad9773/react-nginx:latest 
-##make sure you did docker login
-sudo docker push suryaprasad9773/react-nginx:latest
-
-##recommended script###
-#!/bin/bash
-#cd gold/Gold_Site_Ecommerce
-#sudo git pull
-#sudo docker build -t react-nginx -f goldockerfile .
-#sudo docker tag react-nginx:latest reactdemo/react-nginx:latest ##make sure you did docker login
-#sudo docker push reactdemo/react-nginx:latest
+cd Gold_Site_Ecommerce/git_commit=$(sudo git rev-parse HEAD)
+sudo docker build -t react-nginx:$git_commit -f golddockerfile .
+sudo docker tag react-nginx:$git_commit suryaprasad9773/react-nginx:$git_commit ##make sure you did docker login
+sudo docker push suryaprasad9773/react-nginx:$git_commit
+aws s3 rm s3://gitcommitids1/new_value.txtsudo touch new_value.txt
+sudo chmod 777 new_value.txt
+sudo echo $git_commit > new_value.txt
+aws s3 cp new_value.txt s3://gitcommitids1/
+sudo rm new_value.txt
