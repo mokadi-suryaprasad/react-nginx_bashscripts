f0b635f14707427ea2c176bd87cadbc8257d63c4
Mokadi Surya Prasad <90204775+mokadi-suryaprasad@users.noreply.github.com>
2025-01-25 18:04:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-pods_id=$(microk8s kubectl get pods -n react-microk8s |grep react |awk {'print $1'})
-microk8s kubectl delete pod $pods_id -n react-microk8s 
+cd /home/ubuntu/react-chart/
+aws s3 cp s3://gitcommitids/old_value.txt .
+aws s3 cp s3://gitcommitids/new_value.txt .
+old_value=$(cat old_value.txt)new_value=$(cat new_value.txt)
+sed -i "s/${old_value}/${new_value}/g" values.yamlhelm upgrade react-chart .
+aws s3 rm s3://gitcommitids/old_value.txtecho $new_value > old_value.txt
+aws s3 cp old_value.txt s3://gitcommitids/rm old_value.txt new_value.txt
+
