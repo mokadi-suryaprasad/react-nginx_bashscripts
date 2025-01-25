63e05c2ed05d0d1594802cd78e7f79f4742279c4
Mokadi Surya Prasad <90204775+mokadi-suryaprasad@users.noreply.github.com>
2025-01-25 18:09:48 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/react-chart/
-aws s3 cp s3://gitcommitids/old_value.txt .
-aws s3 cp s3://gitcommitids/new_value.txt .
+aws s3 cp s3://gitcommitids1/old_value.txt .
+aws s3 cp s3://gitcommitids1/new_value.txt .
 old_value=$(cat old_value.txt)new_value=$(cat new_value.txt)
 sed -i "s/${old_value}/${new_value}/g" values.yamlhelm upgrade react-chart .
-aws s3 rm s3://gitcommitids/old_value.txtecho $new_value > old_value.txt
-aws s3 cp old_value.txt s3://gitcommitids/rm old_value.txt new_value.txt
+aws s3 rm s3://gitcommitids1/old_value.txtecho $new_value > old_value.txt
+aws s3 cp old_value.txt s3://gitcommitids1/rm old_value.txt new_value.txt
 
