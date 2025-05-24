e42ba39c0f78b83df26c00c794445f7bb05758b5
Mokadi Surya Prasad <90204775+mokadi-suryaprasad@users.noreply.github.com>
2025-05-24 12:09:27 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
-pods_id=$(microk8s kubectl get pods -n react-nginx |grep react |awk {'print $1'})
-microk8s kubectl delete pod $pods_id -n react-nginx 
+
+set -e  # Exit on error
+
+# Navigate to your Helm chart directory
+cd /home/msuryaprasad11/react-chart
+
+# Download old and new commit ID files from GCS
+gsutil cp gs://gitcommitids/old_value.txt .
+gsutil cp gs://gitcommitids/new_value.txt .
+
+# Read commit IDs
+old_value=$(cat old_value.txt)
+new_value=$(cat new_value.txt)
+
+# Replace the old commit ID with the new one in values.yaml
+sed -i "s/${old_value}/${new_value}/g" values.yaml
+
+# Upgrade the Helm release
+helm upgrade react-chart .
+
+# Update the old_value.txt in GCS and clean up
+gsutil rm gs://gitcommitids/old_value.txt
+echo $new_value > old_value.txt
+gsutil cp old_value.txt gs://gitcommitids/
+
+# Clean local temporary files
+rm old_value.txt new_value.txt
+
+echo "✅ Helm chart updated from $old_value to $new_value and deployed successfully."
